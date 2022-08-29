2d7e437cef24c29e01632d3f196d213235641a0a
Roberto Peruzzo <roberto.peruzzo@studioaqua.it>
2022-08-29 15:01:12 +0200

    fix: corrects the custo ddev build command.

--- .gitpod/project-setup.sh
+++ .gitpod/project-setup.sh
@@ -4,6 +4,4 @@
 if [ ! -d ./web/sites/default/files/translations ] ; then
   mkdir ./web/sites/default/files/translations
 fi
-ddev composer install --prefer-dist --no-interaction
-ddev robo scaffold
-ddev robo build
+ddev build
